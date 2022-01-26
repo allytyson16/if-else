d86111e66a35ba3cadf330d6132cd90df17de3ef
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-01-26 18:00:22 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace Udemyyyyy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+	    if (age == 1)
+		{ 
+			Console.WriteLine("You are 1");
+		}
+		else if (age == 2)
+		{
+			Console.WriteLine("You are 2");
+		}
+		else if (age == 3)
+		{
+			Console.WriteLine("You are 3");
+		}
+		else if (age == 4)
+		{
+			Console.WriteLine("You are 4");
+		}
+		else if(age == 5)
+		{
+			Console.WriteLine("You are 5");
+		}
+		        
     }
 }
